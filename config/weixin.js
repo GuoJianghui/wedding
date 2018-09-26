3dae41adcd771660853e3f1c7cb37aa93df948ed
ripdent <ripdent@hotmail.com>
2018-09-26 23:53:32 +0800

    first commit

---
+++ config/weixin.js
@@ -0,0 +1,6 @@
+module.exports.weixin = {
+  // 微信公众号相关设置
+  appid: 'wxd9d10435d4a5236f',
+  secret: '9d66d23e3964482bc4aa875374cc0136',
+  redirectUri: 'http://pub.tunnel.ripdent.com/home/resolve'
+};
