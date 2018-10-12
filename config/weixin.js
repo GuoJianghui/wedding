460c171210d97e2147050c415326c8e8b9c404c8
ripdent <ripdent@hotmail.com>
2018-10-12 22:56:23 +0800

    提交

--- config/weixin.js
+++ config/weixin.js
@@ -1,8 +1,6 @@
 module.exports.weixin = {
   // 微信公众号相关设置
-  // appid: 'wxd9d10435d4a5236f',
-  // secret: '9d66d23e3964482bc4aa875374cc0136',
   appid: 'wx6697f674df0e46d2',
-  secret: '1838707eda95e879d1a61df063f3768b',
+  secret: '286fef842d6506f4f33a539544abafa8',
   redirectUri: 'http://pub.ngrok.xiaomiqiu.cn/home/resolve'
 };
